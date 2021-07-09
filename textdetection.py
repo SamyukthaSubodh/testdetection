0b8adc6c80f4d586e7b70a6e5d8d8d834ca2d454
SamyukthaSubodh <87174510+SamyukthaSubodh@users.noreply.github.com>
2021-07-09 16:00:29 +0530

    Add files via upload

---
+++ textdetection.py
@@ -0,0 +1,23 @@
+import cv2
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\Tesseract-OCR\\tesseract.exe'
+path='resources/OPENCV.png'
+img1=cv2.imread(path)
+img=cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+hImg,wImg,_=img.shape
+boxes = pytesseract.image_to_data(img)
+for x,b in enumerate(boxes.splitlines()):
+    if x!=0:
+        print(b)
+        b = b.split()
+        print(b)
+        if len(b)==12:
+            x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
+            cv2.rectangle(img, (x,y), (w+x,h+y), (0, 0, 255), 3)
+            cv2.putText(img,b[11] , (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 30, 200), 3)
+print(pytesseract.image_to_string(img1))
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+
